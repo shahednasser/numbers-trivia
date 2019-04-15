80d0b00ec31b9bd546fe840fa1c3371236d2515c
Shahed Nasser <shahednasser@gmail.com>
2019-04-15 14:02:35 +0000

    initial commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Form(props){
+    let [number, setNumber] = useState("random");
+    let [type, setType] = useState("trivia");
+
+    function onNumberChanged(e){
+        setNumber(e.target.value);
+    }
+
+    function onTypeChanged(e){
+        setType(e.target.value);
+    }
+
   return (<form>
             <div>
-              <input type="text" name="number" placeholder="Enter a number (Optional)" />
+              <input type="text" name="number" placeholder="Enter a number (Optional)"
+                    value={number} onChange={onNumberChanged} />
             </div>
             <div>
-              <select name="type">
+              <select name="type" value={type} onChange={onTypeChanged}>
                 <option value="trivia">Trivia</option>
                 <option value="math">Math</option>
                 <option value="date">Date</option>
